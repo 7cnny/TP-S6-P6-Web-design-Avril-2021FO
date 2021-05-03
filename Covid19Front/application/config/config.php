aee176149d69da0261595fef80bd750801facf02
7cnny <75615325+7cnny@users.noreply.github.com>
2021-05-03 23:33:36 +0300

    Update config.php

--- Covid19Front/application/config/config.php
+++ Covid19Front/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/CodeigniterCovid/';
+$config['base_url'] = 'https://front-covid19.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
