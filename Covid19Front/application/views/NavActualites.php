23c218f44db9955a874f5c95bf1e8b19cf28e50f
7cnny <75615325+7cnny@users.noreply.github.com>
2021-05-03 22:46:52 +0300

    Update NavActualites.php

--- Covid19Front/application/views/NavActualites.php
+++ Covid19Front/application/views/NavActualites.php
@@ -5,11 +5,11 @@
 <!DOCTYPE html>
 <html>
     <div id="actualites">
-        <div class="hActus">ACTUALITES</div>
+        <div class="hActus"><h5>ACTUALITES</h5></div>
         <?php for($i=0 ; $i<sizeof($actualites) ;$i++){ ?>
             <a href="covid19-actualite-2021-<?php echo $actualites[$i]['id_actu'] ?>.html" style="text-decoration:none;color:black">
             <div id="SingleActu">           
-                <div class="nomOffre"> <?php echo $actualites[$i]['titre_actu'] ?> </div>
+                <div class="nomOffre"> <h6> <?php echo $actualites[$i]['titre_actu'] ?> </h6> </div>
                 <div class="valeurOffre"> <?php echo $actualites[$i]['preview_actu'] ?> </div>
             </div>
         </a>
