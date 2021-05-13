15e33ea4fddb0706d30c90af4be63a957097fbd0
7cnny <75615325+7cnny@users.noreply.github.com>
2021-05-13 15:19:43 +0300

    Update baseurl

--- Covid19Front/application/config/config.php
+++ Covid19Front/application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:80/OptimisationCovid19Front/';
-//$config['base_url'] = 'https://front-covid19.herokuapp.com';
+//$config['base_url'] = 'http://localhost:80/OptimisationCovid19Front/';
+$config['base_url'] = 'https://front-covid19.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
